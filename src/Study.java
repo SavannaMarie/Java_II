76b73b111bb41eb27f7783c479d023b53448a5a0
Savanna Marie <savannamarie@gmail.com>
2021-09-14 21:36:19 -0500

    Java quiz

--- src/Study.java
+++ src/Study.java
@@ -8,7 +8,31 @@ public class Study {
         return false;
     }
 
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            System.out.println(true);
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            System.out.println(true);
+            return true;
+        }
+        System.out.println(false);
+        return false;
+    }
+
+    public static int sumDouble(int a, int b) {
+        int sum = a + b;
+        if (a == b) {
+            sum = sum * 2;
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
     public static void main(String[] args) {
         sleepIn(true, false);
+        monkeyTrouble(true, false);
+        sumDouble(2,5);
     }
 }
