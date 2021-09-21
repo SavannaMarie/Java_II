1d8fcc2fae26a5d9e45acad3eb8afbb6e5beb37f
Savanna Marie <savannamarie@gmail.com>
2021-09-20 21:25:23 -0500

    Java study

--- src/Study.java
+++ src/Study.java
@@ -44,11 +44,31 @@ public class Study {
         return (talking && (hour < 7 || hour > 20));
     }
 
+    public static boolean makes10(int a, int b) {
+        return (a == 10 || b == 10 || a+b == 10);
+    }
+    public static boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) ||
+                (Math.abs(200 - n) <= 10));
+    }
+
+    public static boolean posNeg(int a, int b, boolean negative) {
+        if (negative) {
+            return (a < 0 && b < 0);
+        }
+        else {
+            return ((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+
     public static void main(String[] args) {
         sleepIn(true, false);
         monkeyTrouble(true, false);
         sumDouble(2,5);
         diff21(15);
         parrotTrouble(true, 5);
+        makes10(3,6);
+        nearHundred(90);
+        posNeg(3, 5, true);
     }
 }
