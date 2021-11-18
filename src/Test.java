c655fa81e580fa115500f665e71e1b78f2bddec1
Garcia <savanna.garcia@cgi.com>
2021-11-18 15:54:47 -0600

    Study

--- src/Test.java
+++ src/Test.java
@@ -1,83 +1,86 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
-
-        //=====Data Types
-        byte smallNumber = 34;
-        System.out.println(smallNumber);
-
-        short notaSmallNumber = 30000;
-
-        int num = 45000000;
-
-        long largeNumber = 14;
-        System.out.println(largeNumber);
-
-        float decimal = 1.5F;
-        System.out.println(decimal);
-
-        double bigDecimal = 45;
-        System.out.println(bigDecimal);
-
-        char singleLetter = 'g';
-
-        boolean amILearningJava = true;
-        System.out.println(amILearningJava);
-
-        String message = "How's it going? \nEverything alright? \n\tJava isn't that scary, is it?";
-        System.out.println(message);
-
-        final  char SINGLE_LETTER = 's';
-        System.out.println(SINGLE_LETTER);
-
-        //====Arithmatic
-        int sum = 4 + 5;
-        System.out.println(sum);
-
-        int difference = 72 - 19;
-        System.out.println(difference);
-
-        int product = 11 * 9;
-        System.out.println(product);
-
-        int quotient = product / sum;
-        System.out.println(quotient);
-
-        int remainder = difference % sum;
-        System.out.println(remainder);
-        int whatWillThisBe = -4 * 2;
-        System.out.println(whatWillThisBe);
-
-        System.out.println(sum++);
-        System.out.println(sum);
-
-        //=====Casting (take a value and roll it over into a small precision)
-        int iAmAnInt = 44;
-        long iAmALong = iAmAnInt;
-        System.out.println(iAmALong);
-
-        iAmALong = Long.MAX_VALUE;
-        System.out.println(++iAmALong);
-        //
-        iAmAnInt = (int)iAmALong;
-        System.out.println(iAmAnInt);
-
-        iAmAnInt = 27;
-        double iAmADouble = iAmAnInt;
-        System.out.println(iAmADouble);
-
-        iAmADouble = 3.141598;
-        iAmAnInt = (int)iAmADouble;
-        System.out.println(iAmAnInt);
-
-        int myFavoriteNumber = 31;
-        System.out.println(myFavoriteNumber);
-
-        String myString = "This is a string!";
-        System.out.println(myString);
-
-        float myNumber = 3.14F;
-        System.out.println(myNumber);
+//        System.out.println("Hello, World!");
+//
+//        //=====Data Types
+//        byte smallNumber = 34;
+//        System.out.println(smallNumber);
+//
+//        short notaSmallNumber = 30000;
+//
+//        int num = 45000000;
+//
+//        long largeNumber = 14;
+//        System.out.println(largeNumber);
+//
+//        float decimal = 1.5F;
+//        System.out.println(decimal);
+//
+//        double bigDecimal = 45;
+//        System.out.println(bigDecimal);
+//
+//        char singleLetter = 'g';
+//
+//        boolean amILearningJava = true;
+//        System.out.println(amILearningJava);
+//
+//        String message = "How's it going? \nEverything alright? \n\tJava isn't that scary, is it?";
+//        System.out.println(message);
+//
+//        final  char SINGLE_LETTER = 's';
+//        System.out.println(SINGLE_LETTER);
+//
+//        //====Arithmatic
+//        int sum = 4 + 5;
+//        System.out.println(sum);
+//
+//        int difference = 72 - 19;
+//        System.out.println(difference);
+//
+//        int product = 11 * 9;
+//        System.out.println(product);
+//
+//        int quotient = product / sum;
+//        System.out.println(quotient);
+//
+//        int remainder = difference % sum;
+//        System.out.println(remainder);
+//        int whatWillThisBe = -4 * 2;
+//        System.out.println(whatWillThisBe);
+//
+//        System.out.println(sum++);
+//        System.out.println(sum);
+//
+//        //=====Casting (take a value and roll it over into a small precision)
+//        int iAmAnInt = 44;
+//        long iAmALong = iAmAnInt;
+//        System.out.println(iAmALong);
+//
+//        iAmALong = Long.MAX_VALUE;
+//        System.out.println(++iAmALong);
+//        //
+//        iAmAnInt = (int)iAmALong;
+//        System.out.println(iAmAnInt);
+//
+//        iAmAnInt = 27;
+//        double iAmADouble = iAmAnInt;
+//        System.out.println(iAmADouble);
+//
+//        iAmADouble = 3.141598;
+//        iAmAnInt = (int)iAmADouble;
+//        System.out.println(iAmAnInt);
+//
+//        int myFavoriteNumber = 31;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "This is a string!";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
 
 //        int x = 5;
 //        System.out.println(x++);
@@ -114,13 +117,30 @@ public class Test {
 //        y = y - x;
 //        System.out.println(x);
 //        System.out.println(y);
+//
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
 
-        int x = 10;
-        int y = 2;
-        x /= y;
-        y -= x;
-        System.out.println(x);
-        System.out.println(y);
+
+        IntStream.generate(() -> random.nextInt(10)).mapToObj(i -> String.valueOf(i))
+
+        final Random random = new Random();
+        IntStream.generate(() -> random.nextInt(10))
+                .mapToObj(i -> String.valueOf(i))
+                .forEach(s -> System.out.println(s));
+        /* Create a IntStream of 10,000 int, all of which are random numbers between 0 and 9
+           Print the following histogram:
+           number : numberOfOccurances
+           Sorted by the largest number of occurances
+         */
+
+
+
+
 
     }
 
