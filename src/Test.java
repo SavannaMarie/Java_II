3b0b52de052a1a6c63cace0140f3118a24eb9967
Savanna Garcia <80721771+SavannaMarie@users.noreply.github.com>
2021-11-19 14:15:56 -0600

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,16 @@
 import java.util.Random;
+
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
 
 public class Test {
     public static void main(String[] args){
 
+//        IntStream.generate(() -> random.nextInt(10)).mapToObj(i -> String.valueOf(i));
 
-//         IntStream.generate(() -> random.nextInt(10)).mapToObj(i -> String.valueOf(i))
+        final Random random = new Random();
 
-//         final Random random = new Random();
-//         IntStream.generate(() -> random.nextInt(10))
-//                 .mapToObj(i -> String.valueOf(i))
-//                 .forEach(s -> System.out.println(s));
-        /* Create a IntStream of 10,000 int, all of which are random numbers between 0 and 9
-           Print the following histogram:
-           number : numberOfOccurances
-           Sorted by the largest number of occurances
-         */
-
-          final Random random = new Random();
 
         IntStream.generate(() -> random.nextInt(10))
                 .limit(10000)
@@ -25,8 +18,17 @@ public class Test {
                 .collect(Collectors.groupingBy(i -> i, Collectors.counting()))
                 .entrySet().stream()
                 .sorted((dis, dat) -> Long.compare(dat.getValue(), dis.getValue()))
-                .forEach((e -> System.out.println(e.getKey() + ": " + e.getValue())));
+                .forEach((System.out::println));
+//                .forEach((e -> System.out.println(e.getKey() + ": " + e.getValue())));
         new Random().ints(10000, 0, 10);
+
+        /* Create a IntStream of 10,000 int, all of which are random numbers between 0 and 9
+           Print the following histogram:
+           number : numberOfOccurances
+           Sorted by the largest number of occurances
+         */
+
+
 
 
 
